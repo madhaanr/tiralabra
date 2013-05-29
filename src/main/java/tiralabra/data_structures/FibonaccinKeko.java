290c223e02c2643e37a5bc9f396801128551ce11
Marko Haanranta <marko.haanranta@helsinki.fi>
2013-05-29 16:24:55 +0300

    fibonaccin kekoa muokattu

--- src/main/java/tiralabra/data_structures/FibonaccinKeko.java
+++ src/main/java/tiralabra/data_structures/FibonaccinKeko.java
@@ -5,7 +5,7 @@ public class FibonaccinKeko {
 
     private FibNode min;
     private int heapSize;
-  
+
     /**
      * Konstruktori luo uuden tyhjän Fibonaccin keon.
      */
@@ -13,116 +13,124 @@ public class FibonaccinKeko {
         min = null;
         heapSize = 0;
     }
-    
+
     /**
      * Metodi palauttaa keon pienimmän alkion key kentän arvon.
+     *
      * @return pienin alkio on tallennettu min muuttujan arvoksi.
      */
     public int min() {
         return min.getKey();
     }
-    
+
     public boolean isEmpty() {
-        return min==null;
+        return min == null;
     }
-    
+
     /**
      * Metodilla lisätään fibonaccin kekoon uusia nodeja.
+     *
      * @param lisattava kekoon lisätään FibNode.
      */
     public void insert(FibNode lisattava) {
         if (min == null) {
-            min=lisattava;         
-        } else {     
+            min = lisattava;
+        } else {
             lisattava.setLeft(min);
             lisattava.setRight(min.getRight());
             min.setRight(lisattava);
             lisattava.getRight().setLeft(lisattava);
-            if (lisattava.getKey() < min.getKey()) {            
+            if (lisattava.getKey() < min.getKey()) {
                 min = lisattava;
             }
         }
         heapSize = heapSize + 1;
     }
-    
+
     /**
-     * Metodi poistaa pienimmän Fibonaccin keon alkion ja tekee käyttää
-     * apu metodina consolidate metodia. Consolidate metodi tekee keosta 
-     * Fibonaccin keon.
-     * @return palauttaa poistettavan alkion.
+     * Metodi poistaa pienimmän Fibonaccin keon alkion ja tekee käyttää apu
+     * metodina consolidate metodia. Consolidate metodi tekee keosta Fibonaccin
+     * keon.
+     * @return palauttaa poistettavan alkion avaimen arvon.
      */
     public int removeMin() {
-        FibNode z = min;
-       
-        if(z!=null) {
-            FibNode x = z.getChild();
+        FibNode vanhaMin = min;
+
+        if (vanhaMin != null) {
+            FibNode kasiteltava = vanhaMin.getChild();
             FibNode temp;
-            for(int i=z.getDegree();i>0;--i) {
-                temp=x.getRight();
-                
-                x.getLeft().setRight(x.getRight());
-                x.getRight().setLeft(x.getLeft());
-                
-                x.setLeft(z);
-                x.setRight(z.getRight());
-                min.setRight(x);
-                x.getRight().setLeft(x);
-                
-                x.setParent(null);
-                x=temp;
-                
+            for (int i = vanhaMin.getDegree(); i > 0; --i) {
+                temp = kasiteltava.getRight();
+                kasiteltava.getLeft().setRight(kasiteltava.getRight());
+                kasiteltava.getRight().setLeft(kasiteltava.getLeft());
+                kasiteltava.setLeft(vanhaMin);
+                kasiteltava.setRight(vanhaMin.getRight());
+                min.setRight(kasiteltava);
+                kasiteltava.getRight().setLeft(kasiteltava);
+                kasiteltava.setParent(null);
+                kasiteltava = temp;
             }
-            //remove z from root list
-            z.getLeft().setRight(z.getRight());
-            z.getRight().setLeft(z.getLeft());
-            if(z==z.getRight()) {
-                min=null;
-            }
-            else {
-                min = z.getRight();
+            //remove vanhaMin from root list
+            vanhaMin.getLeft().setRight(vanhaMin.getRight());
+            vanhaMin.getRight().setLeft(vanhaMin.getLeft());
+            if (vanhaMin == vanhaMin.getRight()) {
+                min = null;
+            } else {
+                min = vanhaMin.getRight();
                 consolidate();
             }
-            heapSize=heapSize-1;
+            heapSize = heapSize - 1;
         }
-            
-        return z.getKey();
+
+        return vanhaMin.getKey();
     }
+
+    /**
+     * Consolidite metodi luo fibonaccin keon. Metodia kutsutaan kun keosta
+     * poistetaan node.
+     */
     public void consolidate() {
         FibNode[] A = new FibNode[1];
         for (int i = 0; i < A.length; ++i) {
-            A[i]=null;
+            A[i] = null;
         }
         FibNode w = min.getRight();
-        while(w!=min) {
-            FibNode x=w;
-            int d=x.getDegree();
-            while(A[d]!=null) {
-                FibNode y=A[d];
-                if(x.getKey()>y.getKey()) {
+        while (w != min) {
+            FibNode x = w;
+            int d = x.getDegree();
+            while (A[d] != null) {
+                FibNode y = A[d];
+                if (x.getKey() > y.getKey()) {
                     FibNode apu = x;
-                    x=y;
-                    y=apu;
+                    x = y;
+                    y = apu;
                 }
-                link(y,x);
-                A[d]=null;
-                d=d+1;
+                link(y, x);
+                A[d] = null;
+                d = d + 1;
             }
-            A[d]=x;
+            A[d] = x;
         }
-        min=null;
+        min = null;
         for (int i = 0; i < A.length; i++) {
-            if(A[i]!=null) {
-                if(min==null) {
-                    min=A[i];
-                }
-                else {
-                    if(A[i].getKey()<min.getKey()) {
-                        min=A[i];
-                    }                       
+            if (A[i] != null) {
+                if (min == null) {
+                    min = A[i];
+                } else {
+                    if (A[i].getKey() < min.getKey()) {
+                        min = A[i];
+                    }
                 }
             }
         }
     }
+    
+    /**
+     * Consolidate metodin apumetodi joka linkittää kaksi nodea. Ekana
+     * annetusta tulee child ja toisena parametrina annetusta parent. 
+     * @param y
+     * @param x
+     */
     public void link(FibNode y, FibNode x) {
         y.getLeft().setRight(y.getRight());
         y.getRight().setLeft(y.getLeft());
@@ -177,23 +185,23 @@ public class FibonaccinKeko {
 //        }
 //        return vanhaMin;
 //    }
- 
+
     public FibonaccinKeko union(FibonaccinKeko fibKeko) {
         FibonaccinKeko keko = new FibonaccinKeko();
-        keko.min=fibKeko.min;
-        
+        keko.min = fibKeko.min;
+
         return keko;
     }
-    
+
     @Override
     public String toString() {
-        String keko="";
+        String keko = "";
         FibNode node = min.getRight();
-        while(node!=min) {
-            keko+=node.getKey();
-            node=node.getRight();
+        while (node != min) {
+            keko += node.getKey();
+            node = node.getRight();
         }
-        keko+=min.getKey();
-        return min.getKey()+"--"+keko+"--"+heapSize;
+        keko += min.getKey();
+        return min.getKey() + "--" + keko + "--" + heapSize;
     }
 }
